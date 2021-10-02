857f83ca674560d766c0eeacac4f8b7ff8f0e5d1
Chris <chris@simplifymy.cloud>
2021-10-02 14:30:03 -0700

    removed sa keys

--- gcp_provider.tf
+++ gcp_provider.tf
@@ -4,10 +4,8 @@
 # ---------------------------------------------------------------------------------------------------------------------
 provider "google" {
   region = "us-west1"
-  credentials = file("~/.config/gcloud/iq9-tf-bootstrap-d8cc9019555f.json")
 }
 
 provider "google-beta" {
   region = "us-west1"
-  credentials = file("~/.config/gcloud/iq9-tf-bootstrap-d8cc9019555f.json")
 }
